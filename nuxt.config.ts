620ae124830beacac5f2c0a35256b75970dea011
Kota Tanaka <te11k.tanaka@gmail.com>
2019-08-10 18:09:55 +0900

    config: TypeScript導入

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
-export default {
+import NuxtConfiguration from '@nuxt/config'
+
+const config: NuxtConfiguration = {
   mode: 'spa',
   /*
    ** Headers of the page
@@ -60,3 +62,5 @@ export default {
     extend(config, ctx) {}
   }
 }
+
+export default config
