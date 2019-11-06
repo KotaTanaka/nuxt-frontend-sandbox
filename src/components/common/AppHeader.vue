d696218c64e4d18ca5edbf2fb01b554ad0e3d718
KotaTanaka <te11k.tanaka@gmail.com>
2019-11-06 13:55:49 +0900

    refactor: nuxt-property-decoratorを使う

--- src/components/common/AppHeader.vue
+++ src/components/common/AppHeader.vue
@@ -4,7 +4,7 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
 
 /**
  * 共通ヘッダー
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,7 +4,7 @@ div
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
+import { Component, Vue } from 'nuxt-property-decorator'
 
 /**
  * トップページ
