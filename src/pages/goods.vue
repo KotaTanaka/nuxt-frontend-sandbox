f9b6ff505eb082eacda9efca176eaed54bc84af2
KotaTanaka <te11k.tanaka@gmail.com>
2019-11-11 21:10:54 +0900

    feat: 商品一覧取得ストアの繋ぎこみ

---
+++ src/pages/goods.vue
@@ -0,0 +1,36 @@
+<template lang="pug">
+div
+  h1 商品一覧
+  GoodsList(:goodsList="goodsList")
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+// from app
+import GoodsList from '@/components/GoodsList.vue'
+
+/**
+ * 商品一覧ページ
+ * @author kotatanaka
+ */
+@Component({
+  layout: 'default',
+  components: {
+    GoodsList
+  }
+})
+export default class GoodsPage extends Vue {
+  async fetch({ store }): Promise<void> {
+    await store.dispatch('goods/fetchGoodsList', {
+      token: '9cb5e40545364a0a928beafbfdd81d6e'
+    })
+  }
+
+  get goodsList() {
+    return this.$store.state.goods.goodsList
+  }
+}
+</script>
+
+<style></style>
