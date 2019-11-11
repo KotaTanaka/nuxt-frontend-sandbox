b73a4919fd6915561de4f894e8af13513e397937
KotaTanaka <te11k.tanaka@gmail.com>
2019-11-11 21:06:34 +0900

    feat: 商品リストコンポーネント作成

---
+++ src/components/GoodsList.vue
@@ -0,0 +1,24 @@
+<template lang="pug">
+div
+  ul
+    li(v-for="goods in goodsList") {{ goods.id }}
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
+
+// from app
+import { IGoodsListElement } from '@/interfaces/api/Goods'
+
+/**
+ * 商品リスト
+ * @author kotatanaka
+ */
+@Component
+export default class GoodsList extends Vue {
+  @Prop({ type: Array, required: true })
+  goodsList: Array<IGoodsListElement> = []
+}
+</script>
+
+<style lang="scss"></style>
