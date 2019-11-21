0805fbd5d259bfee19988f9bd593c9d5fc2cceb2
KotaTanaka <te11k.tanaka@gmail.com>
2019-11-21 19:14:15 +0900

    feat: トップページのスタイリング

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,8 +1,9 @@
 <template lang="pug">
-div
-  h1 トップページ
-  div
-    nuxt-link(to="/goods") 商品一覧
+v-container.top-page
+  h1 商品管理コンソール
+  .links
+    nuxt-link.link(to="/goods") 商品一覧
+    nuxt-link.link(to="/") ページ未作成
 </template>
 
 <script lang="ts">
@@ -18,4 +19,11 @@ import { Component, Vue } from 'nuxt-property-decorator'
 export default class TopPage extends Vue {}
 </script>
 
-<style></style>
+<style lang="scss">
+.top-page {
+  > .links > .link {
+    padding-left: 10px;
+    padding-right: 10px;
+  }
+}
+</style>
