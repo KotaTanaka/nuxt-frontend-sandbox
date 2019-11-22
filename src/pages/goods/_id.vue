4cbf0a65ff13d3ce0719105db90d669c1ba5b7c8
KotaTanaka <te11k.tanaka@gmail.com>
2019-11-22 20:48:11 +0900

    feat: 商品詳細取得ページ繋ぎこみ

--- src/pages/goods/_id.vue
+++ src/pages/goods/_id.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 v-container
   PageHeading(title="商品詳細")
+  p {{ goods.id }}
 </template>
 
 <script lang="ts">
@@ -19,7 +20,18 @@ import PageHeading from '@/components/common/PageHeading.vue'
     PageHeading
   }
 })
-export default class GoodsDetailPage extends Vue {}
+export default class GoodsDetailPage extends Vue {
+  async fetch({ store, route }): Promise<void> {
+    await store.dispatch('goods/fetchGoodsDetail', {
+      token: '9cb5e40545364a0a928beafbfdd81d6e',
+      id: route.params.id
+    })
+  }
+
+  get goods() {
+    return this.$store.state.goods.goods
+  }
+}
 </script>
 
 <style lang="scss"></style>
