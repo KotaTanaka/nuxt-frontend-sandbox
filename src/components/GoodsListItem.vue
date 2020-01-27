45ae76ba2862785cbef6f9ee12a982db29d900f2
KotaTanaka <te11k.tanaka@gmail.com>
2020-01-27 18:58:47 +0900

    feat: 商品削除機能

--- src/components/GoodsListItem.vue
+++ src/components/GoodsListItem.vue
@@ -1,13 +1,20 @@
 <template lang="pug">
-v-card(min-width="300" @click="toDateil")
-  v-card-text
+v-card(min-width="300").item
+  v-card-text(@click="toDateil").content
     div 商品ID: {{ goods.id }}
     p(class="display-1 text--primary") {{ goods.name }}
     p {{ goods.price }} 円
     div(class="text--primary") {{ goods.description }}
   v-card-actions
     v-btn(text) 編集
-    v-btn(text) 削除
+    v-btn(text @click.native.stop="openDeleteModal") 削除
+    ConfirmDialog(
+      :dialog="isDeleteModalVisible"
+      title="商品の削除"
+      message="本当に削除しますか？"
+      @close="closeDeleteModal"
+      @submit="deleteGoods"
+    )
 </template>
 
 <script lang="ts">
@@ -15,22 +22,60 @@ import { Component, Prop, Vue } from 'nuxt-property-decorator'
 import { PAGE_URL } from '@/constants'
 
 // from app
+import ConfirmDialog from '@/components/common/ConfirmDialog.vue'
 import { IGoodsListElement } from '@/interfaces/api/response/Goods'
 
 /**
  * 商品リスト要素
  * @author kotatanaka
  */
-@Component
-export default class GoodsList extends Vue {
-  @Prop({ type: Array, required: true })
+@Component({
+  components: {
+    ConfirmDialog
+  }
+})
+export default class GoodsListItem extends Vue {
+  @Prop({ type: Object, required: true })
   goods: IGoodsListElement
+
+  isDeleteModalVisible = false
 
   /** 詳細画面への遷移 */
   toDateil() {
     this.$router.push(PAGE_URL.GOODS_DETAIL.replace('$id', `${this.goods.id}`))
   }
+
+  /** 商品削除確認 */
+  openDeleteModal() {
+    this.isDeleteModalVisible = true
+  }
+
+  /** 商品削除キャンセル */
+  closeDeleteModal() {
+    this.isDeleteModalVisible = false
+  }
+
+  /** 商品削除の実行 */
+  async deleteGoods() {
+    try {
+      await this.$store.dispatch('goods/deleteGoods', {
+        token: this.$store.state.user.userToken,
+        id: this.goods.id
+      })
+
+      this.isDeleteModalVisible = false
+    } catch (err) {
+      // eslint-disable-next-line no-console
+      console.log(err)
+    }
+  }
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.item {
+  > .content {
+    cursor: pointer;
+  }
+}
+</style>
