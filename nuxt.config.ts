a8e5fcdb079ab059c3a4db8f3cb990d47de51eb3
KotaTanaka <te11k.tanaka@gmail.com>
2020-01-27 15:49:05 +0900

    config: ヘッダ文言設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,14 +18,14 @@ const config: Configuration = {
   srcDir: 'src/',
   /* ページヘッダ */
   head: {
-    title: process.env.npm_package_name || '',
+    title: '商品管理コンソール',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || ''
+        content: '商品管理コンソール'
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
