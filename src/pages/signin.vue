c53f6511bccd07f19edc5bb456e048f9269a4bf9
KotaTanaka <te11k.tanaka@gmail.com>
2020-01-27 16:09:29 +0900

    feat: ログインページ作成

---
+++ src/pages/signin.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+.signin-page
+  v-form(v-model="valid").form
+    v-text-field(
+      v-model="id"
+      :rules="idRules"
+      label="ID"
+      required
+    )
+    v-text-field(
+      v-model="password"
+      :rules="passRules"
+      label="パスワード"
+      required
+    )
+    v-btn(:disabled="!valid" @click="onClick") ログイン
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+/**
+ * ログインページ
+ * @author kotatanaka
+ */
+@Component({
+  layout: 'default'
+})
+export default class SigninPage extends Vue {
+  id = ''
+  password = ''
+  valid = true
+
+  idRules = []
+  passRules = []
+
+  onClick() {}
+}
+</script>
+
+<style lang="scss">
+.signin-page {
+  display: flex;
+  justify-content: center;
+  margin-top: 64px;
+
+  > .form {
+    width: 360px;
+  }
+}
+</style>
