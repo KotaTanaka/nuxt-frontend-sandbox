f9cf5868e265b928f43dee78e8402ebdff611ac6
KotaTanaka <te11k.tanaka@gmail.com>
2020-04-02 18:12:36 +0900

    feat: クッキーからトークンを取得してストアに入れる

--- src/middleware/authentication.ts
+++ src/middleware/authentication.ts
@@ -1,6 +1,6 @@
 import { PAGE_URL } from '@/constants';
 
-export default function ({ store, redirect }) {
+export default function({ store, redirect }) {
   if (!store.state.user.userToken) {
     return redirect(PAGE_URL.LOGIN);
   }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,7 @@
 import { ActionTree, GetterTree, MutationTree } from 'vuex';
+import { Context } from '@nuxt/types';
 import { NuxtAxiosInstance } from '@nuxtjs/axios';
+import cookieparser from 'cookieparser';
 
 // from app
 import { GoodsStore } from './goods';
@@ -16,7 +18,20 @@ export interface RootStore {
 export const mutations: MutationTree<RootStore> = {};
 
 /** Actions */
-export const actions: ActionTree<RootStore, RootStore> = {};
+export const actions: ActionTree<RootStore, RootStore> = {
+  nuxtServerInit({ commit }, { req }: Context): void {
+    if (req.headers.cookie) {
+      const parsedCookie = cookieparser.parse(req.headers.cookie);
+      try {
+        const userToken = parsedCookie.user_token;
+        commit('user/setUserToken', userToken);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }
+    }
+  },
+};
 
 /** Getters */
 export const getters: GetterTree<RootStore, RootStore> = {};
