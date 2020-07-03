444b0a39b2226c5e673686c45221c5850d96733d
KotaTanaka <te11k.tanaka@gmail.com>
2020-07-03 20:18:43 +0900

    feat: 定数をVue,ストア,コンテキストで参照するための型定義

--- src/types/plugins.ts
+++ src/types/plugins.ts
@@ -2,9 +2,30 @@ import { Store } from 'vuex';
 
 // from app
 import { RootStore } from '@/store';
+import * as C from '@/constants';
 
+/** Vueインスタンスのthisで参照するカスタムプラグインの型定義 */
 declare module 'vue/types/vue' {
   interface Vue {
+    /** Store<RootStore> をあてた $store */
     readonly $typedStore: Store<RootStore>;
+    /** 定数 */
+    readonly $C: typeof C;
+  }
+}
+
+/** Vuexストアのthisで参照するカスタムプラグインの型定義 */
+declare module 'vuex/types/index' {
+  interface Store<S> {
+    /** 定数 */
+    readonly $C: typeof C;
+  }
+}
+
+/** コンテキスト(context.app)で参照するカスタムプラグインの型定義 */
+declare module '@nuxt/types' {
+  interface NuxtAppOptions {
+    /** 定数 */
+    readonly $C: typeof C;
   }
 }
