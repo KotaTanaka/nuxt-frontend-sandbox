03a99a108a25a49cabc0250751798392d561c646
KotaTanaka <te11k.tanaka@gmail.com>
2020-09-03 15:27:45 +0900

    storybook: pugとscssを読めるようにする

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,23 @@
 module.exports = {
-  "stories": [
+  stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
+  addons: [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    "@storybook/addon-essentials",
+  ],
+  webpackFinal: async (config) => {
+    config.module.rules.push({
+      test: /\.scss$/,
+      loaders: ['style-loader', 'css-loader', 'sass-loader']
+    });
+
+    config.module.rules.push({
+      test: /\.pug$/,
+      loaders: ['pug-plain-loader']
+    });
+
+    return config;
+  }
 }
