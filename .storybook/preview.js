df7bcd0e78102efb244d5cc62a6591e772c0cb92
KotaTanaka <te11k.tanaka@gmail.com>
2020-09-03 15:28:21 +0900

    storybook: Vuetifyのスタイルがあたるようにする

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,27 @@
+import { addDecorator } from '@storybook/vue'
+import Vue from 'vue';
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+Vue.use(Vuetify);
+
+addDecorator(() => ({
+  vuetify: new Vuetify({
+    customVariables: ['../src/assets/styles/_variables.scss'],
+    theme: {
+      themes: {
+        light: {
+          primary: '#8A0868',
+          secondary: '#B40486',
+          tertiary: '#DF01A5',
+          error: '#b71c1c',
+        },
+      },
+    },
+  }),
+  template: '<v-app><story /></v-app>',
+}));
