290803da115a24e0b24a42dee0a60790a745238a
KotaTanaka <te11k.tanaka@gmail.com>
2021-09-10 14:30:34 +0900

    add: stylelint-config-recess-order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,7 @@
 module.exports = {
   extends: [
     'stylelint-config-standard',
+    'stylelint-config-recess-order',
     'stylelint-config-recommended-scss',
     'stylelint-prettier/recommended'
   ],
