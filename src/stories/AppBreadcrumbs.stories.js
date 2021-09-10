ea812bb9db1a9884c36ec8583caf4ca3acf2a86a
KotaTanaka <te11k.tanaka@gmail.com>
2021-09-10 15:48:23 +0900

    storybook: 見出しとパンくずのカタログ

---
+++ src/stories/AppBreadcrumbs.stories.js
@@ -0,0 +1,23 @@
+import AppBreadcrumbs from '../components/partials/AppBreadcrumbs.vue';
+
+export default {
+  title: 'Partials/AppBreadcrumbs',
+  component: AppBreadcrumbs,
+};
+
+const Template = () => ({
+  props: {
+    breadcrumbList: { type: Array, required: true },
+  },
+  components: { AppBreadcrumbs },
+  template: `<AppBreadcrumbs :breadcrumbList="breadcrumbList" />`,
+});
+
+export const DetailPage = Template.bind({});
+DetailPage.args = {
+  breadcrumbList: [
+    { name: 'トップ', path: '/' },
+    { name: '商品一覧', path: '/goods' },
+    { name: '商品詳細', path: '/goods/1' },
+  ],
+};
